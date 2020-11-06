6ad6d74cfc4bb4fc16330ec10fc10e06e8ccba15
ohjho <jho.yvr+git@gmail.com>
2020-11-06 12:06:00 +0800

    tried using mega upload but non-trival to get file url for ephemeral account

--- main.py
+++ main.py
@@ -1,23 +1,34 @@
 import streamlit as st
 
-import os, sys, json, warnings, base64
-from pytube import YouTube
+import os, sys, json, warnings, base64, pickle, requests
+from pytube import YouTube, extract
 import pandas as pd
+from mega import Mega
+mega = Mega()
 
 from toolbox.st_utils import show_miro_logo
 
-def get_binary_file_downloader_html(bin_file, file_label='File'):
+def is_valid_yt_vid_url(url):
+	try:
+		vid_id = extract.video_id(url)
+		print(vid_id)
+		return True
+	except: # RegexMatchError:
+		return False
+
+def get_binary_file_downloader_html(bin_file, file_label='File', bPickle = False):
 	'''
 	ref: https://discuss.streamlit.io/t/how-to-download-file-in-streamlit/1806/27
 	'''
 	with open(bin_file, 'rb') as f:
 		data = f.read()
+	data = pickle.dumps(data) if bPickle else data
 	bin_str = base64.b64encode(data).decode()
 	href = f'<a href="data:application/octet-stream;base64,{bin_str}" download="{os.path.basename(bin_file)}">Download {file_label}</a>'
 	return href
 
 @st.cache
-def get_dl_link(v_stream, dl_dir = 'output/', filename_prefix = None, bOverwrite = False):
+def get_dl_link(v_stream, dl_dir = 'output/', filename_prefix = None, bOverwrite = False, m_obj = None):
 	fp = os.path.join(dl_dir, filename_prefix + v_stream.default_filename)
 	if not os.path.isfile(fp) or bOverwrite:
 		print(f'Downloading {v_stream.default_filename}')
@@ -25,7 +36,23 @@ def get_dl_link(v_stream, dl_dir = 'output/', filename_prefix = None, bOverwrite
 	else:
 		print(f'video already exists: {fp}')
 	file_type = 'Audio' if v_stream.includes_audio_track and not v_stream.includes_audio_track else 'Video'
-	return get_binary_file_downloader_html(fp, file_type)
+
+	if m_obj:
+		# with open(fp, 'rb') as f:
+		# 	data = f.read()
+		files = {'file': open(fp, 'rb')}
+		response = requests.post('https://file.io', files = files)
+		print(response.status_code)
+		print(response.text)
+
+		# file = m_obj.upload(fp)
+		# print(file)
+		# print(m_obj.export(fp.split('/')[-1]))
+		# #url = m_obj.get_upload_link(file)
+		# url = m_obj.get_link(file)
+		# st.write(f'Download: {url}')
+	else:
+		return get_binary_file_downloader_html(fp, file_type, bPickle = True)
 
 def byte_size_convert(int_bytes, str_unit = 'b'):
 	l_units = ['b', 'kb', 'mb', 'gb']
@@ -56,7 +83,10 @@ def Main():
 	vid_url = st.text_input('Enter Youtube Video URL',
 				value = default_url)
 
-	if vid_url:
+	# m = mega.login()
+	# st.write(m.get_quota())
+
+	if is_valid_yt_vid_url(vid_url):
 		yt = YouTube(vid_url)
 		st.subheader(f'{yt.title}\nlength: `{yt.length}` seconds')
 		st.image(yt.thumbnail_url, use_column_width = True)
@@ -81,8 +111,11 @@ def Main():
 			# 	unsafe_allow_html = True
 			# )
 			st.markdown(
-				get_dl_link(s, filename_prefix = f'itag{itag}_'), unsafe_allow_html = True
+				get_dl_link(s, filename_prefix = f'itag{itag}_', m_obj = None),
+				unsafe_allow_html = True
 			)
+	else:
+		st.warning('please enter a valid YouTube Video URL')
 
 if __name__ == '__main__':
 	Main()
